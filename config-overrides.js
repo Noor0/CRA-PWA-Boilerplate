293ec05517e29e0ca3862968a0b7973d2251196c
Noor ul haq <noor_ul_haq96@hotmail.com>
2019-10-18 17:35:50 +0500

    update webpack plugins config to support custom service worker

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { override, addBabelPlugins } = require('customize-cra');
+const {
+  override,
+  addBabelPlugins,
+  addWebpackPlugin,
+} = require('customize-cra');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = override(
-  ...addBabelPlugins(
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-  ),
-);
+module.exports = function overrideConfigurations(webpack, ...args) {
+  webpack.plugins.pop();
+  const overridenConf = override(
+    ...addBabelPlugins(
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+    ),
+    addWebpackPlugin(
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        globDirectory: webpack.output.path,
+        globPatterns: ['*.{png,ico}'],
+      }),
+    ),
+  )(webpack, ...args);
+
+  return overridenConf;
+};
